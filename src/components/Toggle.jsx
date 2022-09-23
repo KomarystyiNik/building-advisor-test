d278bc649bb67e3f76b5a1c54312038de7d25e46
Nikolay Komarystyi <nikolay.komarystyi@gmail.com>
2022-09-23 09:18:33 +0300

    add vars to toggle component

--- src/components/Toggle.jsx
+++ src/components/Toggle.jsx
@@ -2,6 +2,8 @@ import { useState } from "react"
 
 export default function Toggle() {
   const [isCheck, setIsCheck] = useState(false)
+  const bgSpanStyles = isCheck ? 'justify-end' : 'justify-start'
+  const roundSpanStyles = isCheck ? 'bg-blue-400' : 'bg-white'
 
   const handleInputChange = () => {
     setIsCheck(prevState => !prevState)
@@ -11,12 +13,10 @@ export default function Toggle() {
     <label className="cursor-pointer">
       <input type="checkbox" onChange={handleInputChange} className="hidden" />
       <span
-        className={`flex items-center ${isCheck ? "justify-end" : 'justify-start'} w-9 h-3 bg-grey-100 rounded-xl`}
+        className={`flex items-center ${bgSpanStyles} w-9 h-3 bg-grey-100 rounded-xl`}
       >
         <span
-          className={`inline-block h-5 w-5 ${
-            isCheck ? 'bg-blue-400' : 'bg-white'
-          } shadow-md rounded-full`}
+          className={`inline-block h-5 w-5 ${roundSpanStyles} shadow-md rounded-full`}
         />
       </span>
     </label>
